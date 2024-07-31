c51adb539887469b1047bcf8693fe0e52087475f
ShainaWada <shainah.wada09@gmail.com>
2024-07-31 14:09:49 +0900

    5章まで完了

--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
-import styles from "./index.module.css";
-import { News } from "@/app/_libs/microcms";
-import Category from "../Category";
-import Date from "../Date";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
+
+import styles from './index.module.css';
+import Category from '../Category';
+import Date from '../Date';
+import { News } from '@/app/_libs/microcms';
 
 type Props = {
   news: News[];
@@ -18,15 +19,25 @@ export default function NewsList({ news }: Props) {
       {news.map((article) => (
         <li key={article.id} className={styles.list}>
           <Link href={`/news/${article.id}`} className={styles.link}>
-            <Image
-              src="/no-image.png"
-              alt="No Image"
-              className={styles.image}
-              width={1200}
-              height={630}
-            />
+            {article.thumbnail ? (
+              <Image
+                src={article.thumbnail.url}
+                alt=""
+                className={styles.image}
+                width={article.thumbnail.width}
+                height={article.thumbnail.height}
+              />
+            ) : (
+              <Image
+                className={styles.image}
+                src="/no-image.png"
+                alt="No Image"
+                width={1200}
+                height={630}
+              />
+            )}
             <dl className={styles.content}>
-              <dt className={styles.newsItemTitle}>{article.title}</dt>
+              <dt className={styles.title}>{article.title}</dt>
               <dd className={styles.meta}>
                 <Category category={article.category} />
                 <Date date={article.publishedAt ?? article.createdAt} />
